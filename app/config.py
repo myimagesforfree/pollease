38d0819fc6ccce4a1e00115dfbab0ffb60acf316
Rehill <arehill@obsglobal.com>
2017-01-19 16:02:12 -0600

    Update git ignore. remove print statement

--- app/config.py
+++ app/config.py
@@ -7,10 +7,8 @@ from ConfigParser import ConfigParser
 LOCAL_CONFIG_FILE = '/usr/src/app/config_local.ini'
 PROD_CONFIG_FILE = '/usr/src/app/config.ini'
 
-import os
 def create_config():
     """ Creates a config parser object from local config if present, or production config if not"""
-    print os.listdir(".")
     parser = ConfigParser()
     parser.read(LOCAL_CONFIG_FILE or PROD_CONFIG_FILE)
     return parser
