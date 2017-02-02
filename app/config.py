c58f9b37b0cad4cd0a873a58c6c8d8731e1ed1cd
adamkrieger <adamkrieger.ca@gmail.com>
2017-02-02 13:04:48 -0600

    Implemented load of configuration items via environment variables using SafeConfigParser.

--- app/config.py
+++ app/config.py
@@ -2,15 +2,16 @@
     Manages global configuration for pollease.
 """
 
-from ConfigParser import ConfigParser
+from ConfigParser import SafeConfigParser
+import os
 
-LOCAL_CONFIG_FILE = '/usr/src/app/config_local.ini'
+# LOCAL_CONFIG_FILE = '/usr/src/app/config_local.ini'
 PROD_CONFIG_FILE = '/usr/src/app/config.ini'
 
 def create_config():
     """ Creates a config parser object from local config if present, or production config if not"""
-    parser = ConfigParser()
-    parser.read(LOCAL_CONFIG_FILE or PROD_CONFIG_FILE)
+    parser = SafeConfigParser(os.environ)
+    parser.read(PROD_CONFIG_FILE)
     return parser
 
 CONFIG = create_config()
---
+++ configure.default.sh
@@ -0,0 +1,13 @@
+# Add these lines to your bash profile, or set them in the environment of the build machine.
+
+# Database
+export POLLEASE_DBPATH="/var/pollease.db"
+
+# Logging
+export POLLEASE_PAPERTRAIL_SERVER=""
+export POLLEASE_PAPERTRAIL_PORT="8080"
+
+# Slack
+export POLLEASE_SLACK_AUTH_URL="https://slack.com/api/oauth.access"
+export POLLEASE_SLACK_CLIENT_ID=""
+export POLLEASE_SLACK_CLIENT_SECRET=""
